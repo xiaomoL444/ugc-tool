f68e95c36a3995e526ff3ea9addc039afbd55798
xiaomoL444 <2802273114@qq.com>
2026-03-10 16:11:52 +0800

    更新

--- vue.config.js
+++ vue.config.js
@@ -8,17 +8,5 @@ module.exports = defineConfig({
         path: "path-browserify",
       },
     },
-  },
-  chainWebpack: (config) => {
-    const svgRule = config.module.rule('svg');
- 
-    svgRule.uses.clear();
- 
-    svgRule
-      .use('babel-loader')
-      .loader('babel-loader')
-      .end()
-      .use('vue-svg-loader')
-      .loader('vue-svg-loader');
   }
 })
