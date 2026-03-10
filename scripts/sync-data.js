899cf0b52421b396180e3148ce8f395a1e596724
xiaomoL444 <2802273114@qq.com>
2026-03-10 15:49:19 +0800

    update sync.js

--- scripts/sync-data.js
+++ scripts/sync-data.js
@@ -1,8 +1,24 @@
-const { execSync } = require("child_process")
+const { spawnSync } = require("child_process")
+const fs = require("fs")
 
-console.log("🔄 updating data repo...")
+const repo = "https://github.com/xiaomoL444/ugc-data-repo.git"
+const dir = "public/data"
 
-execSync("git pull", {
-  cwd: "public/data",
-  stdio: "inherit"
-})
+function run(cmd, args, cwd) {
+  const res = spawnSync(cmd, args, {
+    cwd,
+    stdio: "inherit"
+  })
+
+  if (res.status !== 0) {
+    process.exit(res.status)
+  }
+}
+
+if (!fs.existsSync(dir)) {
+  console.log("📦 cloning data repo...")
+  run("git", ["clone", "--depth=1", repo, dir])
+} else {
+  console.log("🔄 updating data repo...")
+  run("git", ["pull"], dir)
+}
